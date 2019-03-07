828fdfe5757c52a38b6bf72faa95136ecd661073
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-03-07 01:53:27 -0500

    Update TestingNB.py

--- GPU-notebook/TestingNB.py
+++ GPU-notebook/TestingNB.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[8]:
-
-
 from tensorflow.python.client import device_lib
 
 def get_available_gpus():
@@ -11,17 +6,7 @@ def get_available_gpus():
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 get_available_gpus()
 
-
-# In[11]:
-
-
-from tensorflow.python.client import device_lib
-
 device_lib.list_local_devices()
-
-
-# In[10]:
-
 
 import tensorflow as tf
 with tf.device('/gpu:0'):
@@ -31,41 +16,17 @@ with tf.device('/gpu:0'):
 
 with tf.Session() as sess:
     print (sess.run(c))
+    
+!pip install sklearn
+!pip install pandas
+!pip install bert-tensorflow
+!pip install tensorflow_hub
+!pip3 install absl-py
 
-
-# In[ ]:
-
-
-get_ipython().system('pip uninstall tensorflow')
-get_ipython().system('pip install tensorflow_gpu')
-get_ipython().system('pip install tensorflow_hub')
-get_ipython().system('pip install sklearn')
-get_ipython().system('pip install bert-tensorflow')
-
-
-# In[6]:
-
-
+Now following https://colab.research.google.com/github/google-research/bert/blob/master/predicting_movie_reviews_with_bert_on_tf_hub.ipynb
+    
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
 from datetime import datetime
-import bert
-from bert import run_classifier
-from bert import optimization
-from bert import tokenization
-
-
-# In[7]:
-
-
-from tensorflow import keras
-import os
-import re
-
-
-# In[ ]:
-
-
-tf.test.gpu_device_name()
-
