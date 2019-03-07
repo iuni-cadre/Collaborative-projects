f041557bcb6c2b3391d066fcce4d3988dea5159c
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-03-06 22:58:33 -0500

    Update AWSjupyterHub.bash

--- GPU-notebook/AWSjupyterHub.bash
+++ GPU-notebook/AWSjupyterHub.bash
@@ -11,3 +11,4 @@ helm init --service-account=tiller
 kubectl -n kube-system delete deployment tiller-deploy
 kubectl delete clusterrolebinding tiller
 kubectl -n kube-system delete serviceaccount tiller
+helm reset --force
