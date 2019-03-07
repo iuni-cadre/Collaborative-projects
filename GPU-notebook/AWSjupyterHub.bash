94814436a63f3fba169bd7bf0436217b34917fc9
XiaoranYan <47752382+XiaoranYan@users.noreply.github.com>
2019-03-06 21:44:38 -0500

    Update AWSjupyterHub.bash

--- GPU-notebook/AWSjupyterHub.bash
+++ GPU-notebook/AWSjupyterHub.bash
@@ -3,3 +3,11 @@ kubectl apply -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v1.1
 kubectl get pods -n kube-system -owide |grep nvid
 
 du -a /home | sort -n -r | head -n 5
+
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account=tiller
+
+kubectl -n kube-system delete deployment tiller-deploy
+kubectl delete clusterrolebinding tiller
+kubectl -n kube-system delete serviceaccount tiller
